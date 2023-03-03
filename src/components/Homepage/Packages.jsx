81f83d106a9887dce81721f02e5d56d23cee2e63
muhammed-swalih <swalihnaduthodi4@gmail.com>
2023-03-04 00:30:06 +0530

    add package

--- src/components/Homepage/Packages.jsx
+++ src/components/Homepage/Packages.jsx
@@ -4,8 +4,8 @@ import flight from '../../TREKK TRAVEL ASSETSS/HOME PAGE/EMOJIS/flighgt.webp'
 import testImage from '../../TREKK TRAVEL ASSETSS/HOME PAGE/IMAGES/testImage.webp'
 function Packages() {
   return (
-    <div className=' w-full h-auto bg-[#EBEBEB] px-5 py-5'>
-        <div>
+    <div className=' w-full h-auto bg-[#EBEBEB]  py-5'>
+        <div className=' px-5'>
             <h1 className=' font-extrabold tracking-wide text-orange-500 uppercase text-sm'>Top Destination</h1>
             <h1 className=' text-2xl font-bold flex inline-flex gap-1'>Explore Top Destination <span><img src={flight} alt="" width={30}/></span></h1>
             {/* <div className=' flex inline-flex justify-center gap-5 py-5 w-full '>
@@ -16,6 +16,11 @@ function Packages() {
                    <img className='  w-full h-full object-cover rounded-lg'  src={testImage} alt="" />
                 </div>
             </div> */}
+           
+        </div>
+        <div className=' flex inline-flex gap-1 overflow-scroll w-full py-5'>
+            <img className=' w-3/4' src={testImage} alt="" />
+            <img className=' w-3/4' src={testImage} alt="" />
         </div>
     </div>
   )
