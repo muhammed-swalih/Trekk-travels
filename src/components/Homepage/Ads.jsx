a58d736fa8a09f332f150c590e60ece00911fda5
muhammed-swalih <swalihnaduthodi4@gmail.com>
2023-03-29 19:46:41 +0530

    commit after changin ads division

--- src/components/Homepage/Ads.jsx
+++ src/components/Homepage/Ads.jsx
@@ -32,8 +32,8 @@ function Ads() {
                     )
                     return (
 
-                        <div className='  w-[800px] h-60 rounded-3xl  mx-auto py-2'>
-                            <img className=' w-full h-full object-cover rounded-3xl' src={`data:image/jpeg;base64,${base64String}`} alt="" />
+                        <div className='  w-[800px] h-[400px] rounded-3xl  mx-auto my-2'>
+                            <img className=' w-full h-[400px] object-cover rounded-3xl' src={`data:image/jpeg;base64,${base64String}`} alt="" />
                         </div>
                     )
                 })}
@@ -71,8 +71,8 @@ function Ads() {
                     String.fromCharCode(...new Uint32Array((items.image.data.data)))
                 )
                 return (
-                    <div className=' w-full h-36 rounded-lg bg-slate-600 my-2'>
-                        <img className='rounded-lg w-full h-full object-cover' src={`data:image/jpeg;base64,${base64String}`} alt="" />
+                    <div className=' w-full h-70 rounded-lg bg-slate-600 my-2'>
+                        <img className='rounded-lg w-full h-70 object-cover' src={`data:image/jpeg;base64,${base64String}`} alt="" />
                     </div>
                 )
             })}
